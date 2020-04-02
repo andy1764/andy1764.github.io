e2c4bf140d9a17b7b793b4cec2ab512457059604
andy1764 <chen1764@gmail.com>
2020-04-02 15:58:32 -0400

    upload 4/2 FC harmony slides

---
+++ b6113c65666718de2b776cce7a603e3961099917/Covariance Harmonization/4-2/macros.js
@@ -0,0 +1,9 @@
+remark.macros.scale = function(w) {
+  var url = this;
+  return '<img src="' + url + '"style="width: ' + w + '" />';
+};
+
+remark.macros.abs = function(width="30%", left="85%", top="15%", cl="") {
+var url = this;
+return '<img src="' + url + '" style="position:absolute;left:' + left + ';top:' + top + ';width:' + width + '" class="' + cl + '" />';
+};
